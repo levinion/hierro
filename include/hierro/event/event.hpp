b9d6f7275b74474d197baec50c8c169f2b5592c4
levinion <levinnion@gmail.com>
2025-04-26 15:13:14 +0800

    add match method for key handling

--- include/hierro/event/event.hpp
+++ include/hierro/event/event.hpp
@@ -3,6 +3,7 @@
 #include "hierro/backend/glfw.hpp"
 #include "hierro/backend/sdl.hpp"
 #include "hierro/event/keycode.hpp"
+#include "magic_enum/magic_enum.hpp"
 
 namespace hierro {
 class ClickEvent {
@@ -19,12 +20,104 @@ private:
 
 class KeyEvent {
 public:
-  Key key;
+  Key key; // lastest key
   bool press; // false for release and true for press
 
   inline bool is_pressed(Key key) {
     return this->keystate->contains(key);
   };
+
+  inline bool match(std::string key1) {
+    if (auto k = magic_enum::enum_cast<Key>(key1); !k)
+      return false;
+    else
+      return is_pressed(k.value());
+  }
+
+  inline bool match(std::string key1, std::string key2) {
+    if (auto k1 = magic_enum::enum_cast<Key>(key1); !k1)
+      return false;
+    else if (auto k2 = magic_enum::enum_cast<Key>(key2); !k2)
+      return false;
+    else
+      return is_pressed(k1.value()) && is_pressed(k2.value());
+  }
+
+  inline bool match(std::string key1, std::string key2, std::string key3) {
+    if (auto k1 = magic_enum::enum_cast<Key>(key1); !k1)
+      return false;
+    else if (auto k2 = magic_enum::enum_cast<Key>(key2); !k2)
+      return false;
+    else if (auto k3 = magic_enum::enum_cast<Key>(key3); !k3)
+      return false;
+    else
+      return is_pressed(k1.value()) && is_pressed(k2.value())
+        && is_pressed(k3.value());
+  }
+
+  inline bool match(
+    std::string key1,
+    std::string key2,
+    std::string key3,
+    std::string key4
+  ) {
+    if (auto k1 = magic_enum::enum_cast<Key>(key1); !k1)
+      return false;
+    else if (auto k2 = magic_enum::enum_cast<Key>(key2); !k2)
+      return false;
+    else if (auto k3 = magic_enum::enum_cast<Key>(key3); !k3)
+      return false;
+    else if (auto k4 = magic_enum::enum_cast<Key>(key4); !k4)
+      return false;
+    else
+      return is_pressed(k1.value()) && is_pressed(k2.value())
+        && is_pressed(k3.value()) && is_pressed(k4.value());
+  }
+
+  inline bool match(
+    std::string key1,
+    std::string key2,
+    std::string key3,
+    std::string key4,
+    std::string key5
+  ) {
+    if (auto k1 = magic_enum::enum_cast<Key>(key1); !k1)
+      return false;
+    else if (auto k2 = magic_enum::enum_cast<Key>(key2); !k2)
+      return false;
+    else if (auto k3 = magic_enum::enum_cast<Key>(key3); !k3)
+      return false;
+    else if (auto k4 = magic_enum::enum_cast<Key>(key4); !k4)
+      return false;
+    else if (auto k5 = magic_enum::enum_cast<Key>(key5); !k5)
+      return false;
+    else
+      return is_pressed(k1.value()) && is_pressed(k2.value())
+        && is_pressed(k3.value()) && is_pressed(k4.value())
+        && is_pressed(k5.value());
+  }
+
+  inline bool match(Key key1) {
+    return is_pressed(key1);
+  }
+
+  inline bool match(Key key1, Key key2) {
+    return is_pressed(key1) && is_pressed(key2);
+  }
+
+  inline bool match(Key key1, Key key2, Key key3) {
+    return is_pressed(key1) && is_pressed(key2) && is_pressed(key3);
+  }
+
+  inline bool match(Key key1, Key key2, Key key3, Key key4) {
+    return is_pressed(key1) && is_pressed(key2) && is_pressed(key3)
+      && is_pressed(key4);
+  }
+
+  inline bool match(Key key1, Key key2, Key key3, Key key4, Key key5) {
+    return is_pressed(key1) && is_pressed(key2) && is_pressed(key3)
+      && is_pressed(key4) && is_pressed(key5);
+  }
 
 private:
   KeyState* keystate;
--- include/hierro/event/keycode.hpp
+++ include/hierro/event/keycode.hpp
@@ -5,7 +5,6 @@
 namespace hierro {
 enum class Key : int {
   Unknown = 0, // UNKNOWN
-
   A = 4,
   B = 5,
   C = 6,
@@ -32,7 +31,6 @@ enum class Key : int {
   X = 27,
   Y = 28,
   Z = 29,
-
   Num1 = 30,
   Num2 = 31,
   Num3 = 32,
@@ -43,13 +41,11 @@ enum class Key : int {
   Num8 = 37,
   Num9 = 38,
   Num0 = 39,
-
   Enter = 40, // Enter
   Escape = 41, // Esc
   Backspace = 42,
   Tab = 43,
   Space = 44,
-
   Minus = 45, // -
   Equals = 46, // =
   LeftBracket = 47, // [
@@ -62,10 +58,7 @@ enum class Key : int {
   Comma = 54, // ,
   Period = 55, // .
   Slash = 56, // /
-
   CapsLock = 57,
-
-  // F1–F12
   F1 = 58,
   F2 = 59,
   F3 = 60,
@@ -78,24 +71,19 @@ enum class Key : int {
   F10 = 67,
   F11 = 68,
   F12 = 69,
-
   PrintScreen = 70,
   ScrollLock = 71,
   Pause = 72,
-
   Insert = 73,
   Home = 74,
   PageUp = 75,
   Delete = 76,
   End = 77,
   PageDown = 78,
-
   ArrowRight = 79,
   ArrowLeft = 80,
   ArrowDown = 81,
   ArrowUp = 82,
-
-  // Pad
   NumLockClear = 83, // NumLock
   KP_Divide = 84,
   KP_Multiply = 85,
@@ -113,8 +101,6 @@ enum class Key : int {
   KP_9 = 97,
   KP_0 = 98,
   KP_Period = 99,
-
-  // Mod
   LCtrl = 224,
   LShift = 225,
   LAlt = 226,
@@ -128,7 +114,7 @@ enum class Key : int {
 };
 
 enum class MouseButton {
-  Unknown = -1, // 未知按键
+  Unknown = -1, // unknown button
   Left = 1, // BUTTON1
   Right = 2, // BUTTON2
   Middle = 3, // BUTTON3
